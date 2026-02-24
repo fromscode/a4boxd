f8985b448fb2e06f5af7e179f0abf24b451a6fc2
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-24 13:09:13 +0000

    Remove unnecessary logs

--- src/controllers/genres.ts
+++ src/controllers/genres.ts
@@ -3,7 +3,6 @@ import queries from "../db/queries.js";
 
 async function getGenre(req: Request, res: Response) {
     const genreId = +(req.params.genreId as string);
-    console.log(genreId);
 
     const movies = await queries.getMoviesByGenreId(genreId);
     const genre = res.locals.genres.find((genre: any) => genre.id === genreId);
@@ -11,8 +10,6 @@ async function getGenre(req: Request, res: Response) {
         genre: genre,
         movies: movies,
     };
-    console.log(res.locals.genres);
-    console.log(genre);
     res.render("index", renderData);
 }
 
