0b3f0770c4cf4b7892c2005f8d61752de9592b4a
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-24 13:22:19 +0000

    Add tailwind and daisyui deps

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(import.meta.dirname, "..", "views"));
 
+app.use(express.static(path.join(import.meta.dirname, "..", "public")));
+
 app.use(genreCache);
 app.use(indexRouter);
 app.use("/genres", genreRouter);
